a22014594efd0dfb10f5fac036461370def9098c
Icesythe7 <lucasloveless90@gmail.com>
2019-05-09 01:09:15 -0400

    Added comments to functions

--- MorphTutorial/Lua.cpp
+++ MorphTutorial/Lua.cpp
@@ -2,41 +2,86 @@
 
 namespace Lua
 {
+	/// <summary>
+	/// Returns the number of params for the lua function that was called.
+	/// </summary>
+	/// <param name="luaState">The luaState.</param>
+	/// <returns>The number of params.</returns>
 	int32_t GetTop(const uintptr_t luaState)
 	{
 		return reinterpret_cast<int32_t(__cdecl*)(uintptr_t)>(WowAddresses::LuaGetState)(luaState);
 	}
 
+	/// <summary>
+	/// Returns a boolean if the specified element can be converted to a string.
+	/// </summary>
+	/// <param name="luaState">The luaState.</param>
+	/// <param name="stackPos">The position of the element to check on the stack.</param>
+	/// <returns>True if the specified stack element can be converted to a string.</returns>
 	bool IsString(const uintptr_t luaState, const int32_t stackPos)
 	{
 		return reinterpret_cast<bool(__cdecl*)(uintptr_t, int32_t)>(WowAddresses::LuaIsString)(luaState, stackPos);
 	}
 
+	/// <summary>
+	/// Converts the specified element in lua to a const char*.
+	/// </summary>
+	/// <param name="luaState">The luaState.</param>
+	/// <param name="stackPos">The position of the element to check on the stack.</param>
+	/// <returns>A const char* of the lua string.</returns>
 	const char* ToString(const uintptr_t luaState, const int32_t stackPos)
 	{
 		return reinterpret_cast<const char*(__cdecl*)(uintptr_t, int32_t, DWORD*)>(WowAddresses::LuaToString)(luaState, stackPos, nullptr);
 	}
 
+	/// <summary>
+	/// Returns a boolean if the specified element can be converted to a number.
+	/// </summary>
+	/// <param name="luaState">The luaState.</param>
+	/// <param name="stackPos">The position of the element to check on the stack.</param>
+	/// <returns>True if the specified stack element can be converted to a number.</returns>
 	bool IsNumber(const uintptr_t luaState, const int32_t stackPos)
 	{
 		return reinterpret_cast<bool(__cdecl*)(uintptr_t, int32_t)>(WowAddresses::LuaIsNumber)(luaState, stackPos);
 	}
 
+	/// <summary>
+	/// Converts the specified element in lua to a double.
+	/// </summary>
+	/// <param name="luaState">The luaSate.</param>
+	/// <param name="stackPos">The position of the element to check on the stack.</param>
+	/// <returns>A double of the lua number.</returns>
 	double ToNumber(const uintptr_t luaState, const int32_t stackPos)
 	{
 		return reinterpret_cast<double(__cdecl*)(uintptr_t, int32_t)>(WowAddresses::LuaToNumber)(luaState, stackPos);
 	}
 
+	/// <summary>
+	/// Throws an error in lua.
+	/// </summary>
+	/// <param name="luaState">The luaState.</param>
+	/// <param name="msg">The error message to display.</param>
 	void Error(const uintptr_t luaState, const char* msg)
 	{
 		reinterpret_cast<void(__cdecl*)(uintptr_t, const char*, int32_t)>(WowAddresses::LuaError)(luaState, msg, 0);
 	}
 
+	/// <summary>
+	/// Helper function to compare two strings.
+	/// </summary>
+	/// <param name="string1">The first string.</param>
+	/// <param name="string2">The second string.</param>
+	/// <returns>True if both strings are an exact match.</returns>
 	bool CompareStrings(const char* string1, const char* string2)
 	{
 		return strcmp(string1, string2) == 0;
 	}
 
+	/// <summary>
+	/// Updates the model of the unit specified in lua.
+	/// </summary>
+	/// <param name="l">The luaState passed by lua when called.</param>
+	/// <returns>The number of return values passed to the lua function from C++.</returns>
 	int64_t UpdateModel(const uintptr_t l)
 	{
 		const auto numArgs = GetTop(l);
@@ -63,6 +108,11 @@ namespace Lua
 		return 0;
 	}
 
+	/// <summary>
+	/// Sets the item ids for the specified unit and slot.
+	/// </summary>
+	/// <param name="l">The luaState passed by lua when called.</param>
+	/// <returns>The number of return values passed to the lua function from C++.</returns>
 	int64_t SetVisibleItem(const uintptr_t l)
 	{
 		const auto numArgs = GetTop(l);
